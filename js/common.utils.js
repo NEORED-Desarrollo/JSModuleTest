63cd83249bac0035ae297944a4759e8897601636
Luis Acxis <acxis@neored.com.mx>
2024-11-06 16:41:38 -0600

    First commit

---
+++ js/common.utils.js
@@ -0,0 +1,7 @@
+export const saludo = (name) => {
+    alert(name);
+}
+
+export const saludo2 = (name) => {
+    saludo(name);
+}
---
+++ js/utils.js
@@ -0,0 +1,4 @@
+import { saludo, saludo2 } from './common.utils.js';
+
+window.saludar = saludo;
+window.saludar2 = saludo2;
